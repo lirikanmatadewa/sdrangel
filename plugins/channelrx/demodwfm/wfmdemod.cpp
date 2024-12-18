18a03d77fc5d447066cacf13d4e6ee0a034337e8
Wisjnu Sjarifuddin <wisjnu.sjarifuddin@rohde-schwarz.com>
2024-12-18 15:40:31 +0800

    -cleaning source

--- plugins/channelrx/demodwfm/wfmdemod.cpp
+++ plugins/channelrx/demodwfm/wfmdemod.cpp
@@ -238,11 +238,6 @@ void WFMDemod::applySettings(const WFMDemodSettings& settings, bool force)
             << " m_reverseAPIChannelIndex: " << settings.m_reverseAPIChannelIndex
             << " force: " << force;
 
-    /*int toneValue = GLSpectrum::getTone();
-    m_settings.setTone(toneValue);
-    qDebug() << "----------->> ToneValue :: " << toneValue;*/
-
-
     QList<QString> reverseAPIKeys;
 
     if((settings.m_inputFrequencyOffset != m_settings.m_inputFrequencyOffset) || force) {
