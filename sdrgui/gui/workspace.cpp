238e838fd1c0ee08cc79978924f91b778446698b
Wisjnu Sjarifuddin <wisjnu.sjarifuddin@rohde-schwarz.com>
2026-05-18 20:59:55 +0800

    - Auto set BaldeRF2 if attached without device selection - If not attached yet, it will re-scan again NOTE: After attach and detect and remove, will crash since the list detection somehow can not detect the disconnection status. Probably bug in the SDRangel itself.

--- sdrgui/gui/workspace.cpp
+++ sdrgui/gui/workspace.cpp
@@ -30,6 +30,7 @@
 #include <QApplication>
 #include <QMenu>
 #include <QAction>
+#include <QMessageBox>
 
 #include "gui/samplingdevicedialog.h"
 #include "gui/rollupcontents.h"
@@ -371,9 +372,32 @@ void Workspace::addRxDeviceClicked()
 {
     SamplingDeviceDialog dialog(0, this);
 
-    if (dialog.exec() == QDialog::Accepted) {
-        emit addRxDevice(this, dialog.getSelectedDeviceIndex());
+	m_addRxDeviceButton->setDisabled(true);
+	QString searchString = "BladeRF2[0:0]";
+	QMap<int, QString> deviceMap = dialog.getDeviceMap();
+	QList<int> matchingKeys;
+
+	for (auto it = deviceMap.begin(); it != deviceMap.end(); ++it) {
+		if (it.value().contains(searchString)) {
+			matchingKeys.append(it.key());
+		}
+	}
+	if (matchingKeys.isEmpty()) {
+		QMessageBox msgBox;
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.setText("Please plug-in the BladerRF2.");
+		msgBox.setStandardButtons(QMessageBox::Ok);
+		msgBox.setDefaultButton(QMessageBox::Ok);
+		msgBox.exec();
+	}
+	else {
+		dialog.setSelectedDeviceIndex(matchingKeys[0]);
+		emit addRxDevice(this, matchingKeys[0]);
     }
+
+	m_addRxDeviceButton->setDisabled(false);
+
+	/* if (dialog.exec() == QDialog::Accepted) { emit addRxDevice(this, dialog.getSelectedDeviceIndex()); } */
 }
 
 void Workspace::addTxDeviceClicked()
